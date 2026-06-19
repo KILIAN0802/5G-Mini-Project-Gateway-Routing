959f4c9991ec446fd5ede1277409f0b369f77f3c
nguyendat <nguyendat08022004@gmail.com>
2026-06-19 20:33:25 +0700

    ActiveRequest should not be overwriten by multi-thread at the sametime -> use int32 instead of int -> required type of atomic

--- gateway/models/instance.go
+++ gateway/models/instance.go
@@ -6,5 +6,5 @@ type Instance struct {
 	Healthy       bool
 	Weight        int
 	CurrentWeight int
-	ActiveRequest int 
+	ActiveRequest int32 // Kiểu dữ liệu chính xác 32 bit khác với int có thể là 32 hoặc 64
 }
